d6e43865de5225f08060b61f93f9aeef8ea7e9f4
v1nc <github@reckendrees.systems>
2020-07-14 21:43:54 +0200

    change image day

---
+++ apps/animclk/cycle.js
@@ -0,0 +1,19 @@
+Files written
+Cycles [
+  { reverse: 0, rate: 0, low: 232, high: 237 },
+  { reverse: 0, rate: 0, low: 167, high: 174 },
+  { reverse: 0, rate: 1536, low: 135, high: 143 },
+  { reverse: 0, rate: 1380, low: 127, high: 134 },
+  { reverse: 0, rate: 2304, low: 119, high: 126 },
+  { reverse: 0, rate: 1536, low: 217, high: 223 },
+  { reverse: 0, rate: 2841, low: 210, high: 216 },
+  { reverse: 0, rate: 2841, low: 203, high: 209 },
+  { reverse: 0, rate: 2841, low: 196, high: 202 },
+  { reverse: 0, rate: 2841, low: 189, high: 195 },
+  { reverse: 0, rate: 2841, low: 182, high: 188 },
+  { reverse: 0, rate: 2841, low: 175, high: 181 },
+  { reverse: 0, rate: 0, low: 0, high: 0 },
+  { reverse: 0, rate: 0, low: 0, high: 0 },
+  { reverse: 0, rate: 0, low: 0, high: 0 },
+  { reverse: 0, rate: 0, low: 0, high: 0 }
+]
